aa3d4e32b706d5eded0d05eeccf779b8e994eebd
SyedAthar25 <sayedather03@gmail.com>
2025-10-01 01:43:46 +0530

    final commit

--- InventoryApi/obj/Debug/net9.0/InventoryApi.AssemblyInfo.cs
+++ InventoryApi/obj/Debug/net9.0/InventoryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380ebefa909219cd2c402010e8e9a0de7a764f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1f0f0250466e1fddf76d54970133d4bd5ee11c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
